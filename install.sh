f8f663573f0e426191d61d33a29145bce0185f66
st3inum <st3inum@gmail.com>
2023-01-24 13:30:16 +0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -11,6 +11,8 @@ sudo iptables -A INPUT -s 172.104.169.81 -j ACCEPT
 sudo iptables -A INPUT -s 172.104.160.52 -j ACCEPT
 sudo iptables -A INPUT -s 8.8.8.8 -j ACCEPT
 sudo cp /etc/iptables/rules.v4 /etc/iptables.rules
+sudo sh -c "iptables-save > /etc/iptables/rules.v4"
+sudo sh -c "iptables-save > /etc/iptables.rules"
 
 sudo echo "gsettings set org.gnome.desktop.interface gtk-theme \"Yaru-bark\"" >> /etc/profile
 sudo echo "gsettings set org.gnome.desktop.background picture-options 'scaled'" >> /etc/profile
